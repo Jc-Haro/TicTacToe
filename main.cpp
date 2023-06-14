f51ce38fa13acc426af634557023e860a7ebe258
Jc-Haro <jcharomo@gmail.com>
2023-06-14 16:32:31 -0600

    "WIn check"

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<iomanip>
 #include<vector>
 #include<time.h>
+#include<string>
 
 const int NUM_SQUARES = 9;
 const char EMPTY = ' ';
@@ -26,9 +27,13 @@ int main(){
     char IA;
 
     int playerInput{};
+    int IA_Input{};
 
     std::vector<char> board (NUM_SQUARES,EMPTY);
     std::vector<int> availableBoxes = {0,1,2,3,4,5,6,7,8};
+    char winState = 'N';
+    //First turn is X turn
+    char actualTurn = PLAYER_X;
 
     Welcome();
 
@@ -36,15 +41,49 @@ int main(){
     player = GetPlayerSymbol();
     IA = player==PLAYER_X? PLAYER_O : PLAYER_X;
 
-    std::cout<<"\n\nPlayer:"<<player<<"\n\n";
-    std::cout<<"\n\nPlayer"<<IA<<"\n\n";
-    do{
-        DrawChoiceBoard(board);  
-        playerInput = getPlayerInput(availableBoxes);
-        board[playerInput] = player;
-    }while(availableBoxes.size()>0);
+    std::cout<<"\nPlayer: "<<player<<std::endl;
+    std::cout<<"IA: "<<IA<<std::endl;
 
-    DrawChoiceBoard(board);  
+
+    while(winState == NO_ONE){ 
+       
+        DrawChoiceBoard(board); 
+        if(actualTurn == player){
+            std::cout<<std::endl<<"It is your turn, pick a box"<<std::endl;
+            playerInput = getPlayerInput(availableBoxes);
+            board[playerInput] = player;
+            actualTurn = IA;
+        }
+        else{
+            std::cout<<std::endl<<"It is my turn"<<std::endl;
+            int randomIAIndex = rand()%availableBoxes.size();
+            IA_Input = availableBoxes[randomIAIndex];
+            board[IA_Input] = IA;
+            availableBoxes.erase(availableBoxes.begin() + randomIAIndex);
+            actualTurn = player;
+        }
+        
+        
+        winState = winner(board);
+
+    } 
+    std::cout<<"\nFinal board"<<std::endl;
+    DrawChoiceBoard(board); 
+    if(winState == TIE){
+        std::cout<<"\nIt's a Tie"<<std::endl;
+        //Add salty IA rechallenge
+    }
+    else{
+        if(winState == IA){
+            std::cout<<"And I have won!!"<<std::endl;
+            //Add a random IA win phrase
+        }
+        else{
+            std::cout<<"And you have won, what a a surprise!!";
+            //Add salty IA loose phares 
+        }
+    }
+
     
     return 0;
 }
@@ -60,7 +99,7 @@ char GetPlayerSymbol(){
     srand(static_cast<unsigned int>(time(0)));
     int randomIndex = rand()%startPhrases.size();
     std::cout<<startPhrases[randomIndex];
-    return AskYesNo("do you want to start?")? PLAYER_X : PLAYER_O;
+    return AskYesNo(" do you want to start?")? PLAYER_X : PLAYER_O;
 
 }
 bool AskYesNo(std::string question)
@@ -69,7 +108,7 @@ bool AskYesNo(std::string question)
 
     do {
         std::cout << question << "(y/n)";
-        getline(std::cin, answer);
+        std::getline(std::cin, answer);
 
     } while (answer[0] != 'y' && answer[0] != 'n');
 
@@ -126,7 +165,7 @@ int  checkValidInput(){
     bool isValid = false;
    
     do{
-        
+
        std::getline(std::cin,temporalPlayerInput);
        isValid = checkDigit(temporalPlayerInput[0]);
        if(!isValid){
@@ -142,4 +181,35 @@ bool checkDigit(char check){
 }
 char winner(const std::vector<char>  & board){
 
+    //Vertical lines check
+    for(int i = 0; i< board.size(); i +=3){
+        if(board[i]!= EMPTY && board[i] == board[i+1] && board[i+1] == board[i+2]){
+        return board[i];
+    }
+    }
+    //Horizontal lines check
+    for(int i = 0; i< board.size(); i++){
+        if(board[i]!= EMPTY && board[i] == board[i+3] && board[i+3] == board[i+6]){
+        return board[i];
+    }
+    }
+    //Diagonal check
+    if(board[0]!= EMPTY && board[0] == board[4] && board[4] == board[8]){
+        return board[0];
+    }
+     if(board[2]!= EMPTY && board[2] == board[4] && board[4] == board[6]){
+        return board[2];
+    }
+
+    //Tie check
+    for(int i  = 0; i<board.size(); i++){
+        if(board[i] == EMPTY){
+            return 'N';
+        }
+    }
+
+    //Default
+    return 'T';
+   
 }
+
